6527a60f1adb0510b443b8fdca2d3970b403b025
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-05-23 08:32:10 -0700

    update activation delay in tests

--- src/citycoin-client.ts
+++ src/citycoin-client.ts
@@ -24,7 +24,7 @@ export enum ErrCode {
   ERR_MINING_ACTIVATION_THRESHOLD_REACHED
 }
 
-export const MINING_ACTIVATION_DELAY = 100;
+export const MINING_ACTIVATION_DELAY = 500;
 export const FIRST_STACKING_BLOCK = 1 + MINING_ACTIVATION_DELAY;
 export const REWARD_CYCLE_LENGTH = 500;
 export class CityCoinClient {
