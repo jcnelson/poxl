ece0c3ea3117f1d35cba5de2835a450419dcd10f
Jason Schrader <whoabuddy@users.noreply.github.com>
2021-07-26 06:09:39 -0700

    feat: add tests based on changing city wallet from external contract

---
+++ tests/test-utils.test.ts
@@ -0,0 +1,34 @@
+import { assertEquals, describe, Tx, types } from "../deps.ts";
+import { TestUtilsClient } from "../src/test-utils-client.ts";
+import { it } from "../src/testutil.ts";
+
+describe("[CityCoin Test Utils]", () => {
+  describe("test-wallet-attack()", () => {
+    it("throws ERR_UNAUTHORIZED when changing city wallet to a new address", (chain, accounts, clients) => {
+      //arrange
+      const sender = accounts.get("wallet_1")!;
+
+      // act
+      const block = chain.mineBlock([
+        clients.testUtils.testWalletAttack(sender),
+      ]);
+
+      // assert
+      block.receipts[0].result.expectErr().expectUint(1000);
+    });
+  });
+  describe("test-wallet-attack-as-contract()", () => {
+    it("throws ERR_UNAUTHORIZED when changing city wallet to a new address as a contract", (chain, accounts, clients) => {
+      //arrange
+      const sender = accounts.get("wallet_1")!;
+
+      // act
+      const block = chain.mineBlock([
+        clients.testUtils.testWalletAttackAsContract(sender),
+      ]);
+
+      // assert
+      block.receipts[0].result.expectErr().expectUint(1000);
+    });
+  });
+});
